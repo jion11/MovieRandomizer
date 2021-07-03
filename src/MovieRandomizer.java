00758530295e883588d61b4b8a66243033b95793
Cameron <cameron.ohree@gmail.com>
2021-07-02 23:52:06 -0400

    Test

--- src/MovieRandomizer.java
+++ src/MovieRandomizer.java
@@ -1,7 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Scanner;
+
 public class MovieRandomizer {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
-        System.out.println("Hello World");
+
+        File movieList = new File("D:\\CodingProjects\\Java\\Java Projects\\Movie Randomizer\\MovieRandomizer\\MoviesToWatch.txt");
+        Scanner myReader = new Scanner(movieList);
+        while (myReader.hasNextLine()) {
+            String movieName = myReader.nextLine();
+            System.out.println(movieName);
+            System.out.println("Hi");
+        }
+        myReader.close();
+
+
     }
 }
